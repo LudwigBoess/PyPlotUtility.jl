44c240c76e3d5625b396dc0ee57b5f250227d334
LudwigBoess <lboess@usm.lmu.de>
2021-03-04 09:05:57 +0100

    new colorbar options

--- src/plot_utility/colorbar.jl
+++ src/plot_utility/colorbar.jl
@@ -40,6 +40,82 @@ function get_colorbar_top(ax::PyCall.PyObject, im::PyCall.PyObject,
 end
 
 """
+    get_colorbar_top(ax::PyCall.PyObject, im::PyCall.PyObject, 
+                     label::AbstractString, 
+                     axis_label_font_size::Integer, 
+                     tick_label_size::Integer)
+
+Returns a colorbar at the top of an plot.
+"""
+function get_colorbar_left(ax::PyCall.PyObject, im::PyCall.PyObject, 
+                            label::AbstractString, 
+                            axis_label_font_size::Integer, 
+                            tick_label_size::Integer)
+
+    axes_divider = pyimport("mpl_toolkits.axes_grid1.axes_divider")
+    
+    ax_divider = axes_divider.make_axes_locatable(ax)
+    cax = ax_divider.append_axes("left", size="7%", pad="2%")
+    cb = colorbar(im, cax=cax)#, fraction=0.046, pad=0.5)#, cax=cbaxis1)
+    cb.set_label(label, fontsize=axis_label_font_size)
+    cb.ax.tick_params(
+                        direction="in",
+                        which="major",
+                        labelsize=tick_label_size,
+                        size=6, width=1
+                        )
+    cb.ax.tick_params(
+                        direction="in",
+                        which="minor",
+                        labelsize=tick_label_size,
+                        size=3, width=1
+                        )
+
+    cax.xaxis.set_ticks_position("left")
+    cax.xaxis.set_label_position("left")
+
+    return cax
+end
+
+"""
+    get_colorbar_top(ax::PyCall.PyObject, im::PyCall.PyObject, 
+                     label::AbstractString, 
+                     axis_label_font_size::Integer, 
+                     tick_label_size::Integer)
+
+Returns a colorbar at the top of an plot.
+"""
+function get_colorbar_right(ax::PyCall.PyObject, im::PyCall.PyObject, 
+                            label::AbstractString, 
+                            axis_label_font_size::Integer, 
+                            tick_label_size::Integer)
+
+    axes_divider = pyimport("mpl_toolkits.axes_grid1.axes_divider")
+    
+    ax_divider = axes_divider.make_axes_locatable(ax)
+    cax = ax_divider.append_axes("right", size="7%", pad="2%")
+    cb = colorbar(im, cax=cax)#, fraction=0.046, pad=0.5)#, cax=cbaxis1)
+    cb.set_label(label, fontsize=axis_label_font_size)
+    cb.ax.tick_params(
+                        direction="in",
+                        which="major",
+                        labelsize=tick_label_size,
+                        size=6, width=1
+                        )
+    cb.ax.tick_params(
+                        direction="in",
+                        which="minor",
+                        labelsize=tick_label_size,
+                        size=3, width=1
+                        )
+
+    cax.xaxis.set_ticks_position("right")
+    cax.xaxis.set_label_position("right")
+
+    return cax
+end
+
+"""
     shift_colorbar_label!(cax1::PyCall.PyObject, shift::String)
 
 Shift a colorbar label at the left `["left", "l"]` of the colorbar to the right, or vice versa with `["right", "r"]`.

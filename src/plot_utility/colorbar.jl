6ba6c36c071853b5bab20a64b81fe44a24d9c4bb
Ludwig Böss <ludwig.boess@outlook.de>
2023-04-21 21:55:32 +0200

    streamline syntax

--- src/plot_utility/colorbar.jl
+++ src/plot_utility/colorbar.jl
@@ -120,26 +120,28 @@ function get_colorbar_right(ax::PyCall.PyObject, im::PyCall.PyObject,
 end
 
 """
-    shift_colorbar_label!(cax1::PyCall.PyObject, shift::String)
+    shift_colorbar_label!(cax::PyCall.PyObject, shift::String)
 
 Shift a colorbar label at the left `["left", "l"]` of the colorbar to the right, or vice versa with `["right", "r"]`.
 """
-function shift_colorbar_label!(cax1::PyCall.PyObject, shift::String)
+function shift_colorbar_label!(cax::PyCall.PyObject, shift::String)
 
     if shift ∈ ["left", "l"]
-        label1 = cax1.xaxis.get_majorticklabels()
-        label1[end].set_horizontalalignment("right")
+        label1 = cax.xaxis.get_majorticklabels()
+        label1[end].set_ha("right")
     elseif shift ∈ ["right", "r"]
-        label1 = cax1.xaxis.get_majorticklabels()
-        label1[1].set_horizontalalignment("left")
+        label1 = cax.xaxis.get_majorticklabels()
+        label1[1].set_ha("left")
     elseif shift ∈ ["top", "t"]
-        label1 = cax1.yaxis.get_majorticklabels()
+        label1 = cax.yaxis.get_majorticklabels()
         label1[1].set_verticalalignment("bottom")
     elseif shift ∈ ["bottom", "b"]
-        label1 = cax1.yaxis.get_majorticklabels()
+        label1 = cax.yaxis.get_majorticklabels()
         label1[end].set_verticalalignment("top")
     else
         error("Shift needs to be 'left'/'l', 'right'/'r' 'top / t' or 'bottom / b' !")
     end
+
+    return cax
 
 end
